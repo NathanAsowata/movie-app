3fdb7b255c1ab34c46052e820cd0c74821061b0d
Nathan Asowata <nathanasowata@gmail.com>
2023-02-24 12:13:29 +0100

    style: added styling for the search result page

--- src/pages/search/[userInput].tsx
+++ src/pages/search/[userInput].tsx
@@ -31,38 +31,64 @@ const SearchResult = () => {
 
     if(isLoading) {
         // This is a shimmer effect for the loading state
-        return (
-          <ContentLoader
-            speed={2}
-            width={300}
-            height={100}
-            viewBox="0 0 500 60"
-            backgroundColor="#1E293B"
-            foregroundColor="#0F172A"
-            uniqueKey="shimmer"
-            className="flex"
-          >
-            <rect x="0" y="0" rx="3" ry="3" width="500" height="250" />
-          </ContentLoader>
-        )
+        return ( 
+            <>
+                <Head>
+                    <title>{userInput} - Search Results</title>
+                    <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+                </Head>
+                <main className="grid place-content-center min-h-[95vh] bg-slate-800">
+                    <ContentLoader
+                        speed={2}
+                        width={300}
+                        height={100}
+                        viewBox="0 0 500 60"
+                        backgroundColor="#1E293B"
+                        foregroundColor="#0F172A"
+                        uniqueKey="shimmer"
+                        className="flex"
+                    >
+                        <rect x="0" y="0" rx="3" ry="3" width="500" height="250" />
+                    </ContentLoader>
+                </main>
+            </>
+            )
     }
 
     if(isError) {
-        return <h1>An error occurred</h1>
+        return ( 
+            <>
+                <Head>
+                    <title>{userInput} - Search Results</title>
+                    <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+                </Head>
+                <main className="grid place-content-center min-h-[95vh] bg-slate-800">
+                    <h1 className="text-white">An error occured. Try again in a few minutes</h1>
+                </main>
+            </>
+            )
     }
 
-    // if(!data.results){
-    //     return <h1>We could not find that movie. Please search for another movie</h1>
-    // }
+    if(data.results.length === 0){
+       return ( 
+        <>
+            <Head>
+                <title>No Results Found</title>
+                <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+            </Head>
+            <main className="grid place-content-center min-h-[95vh] bg-slate-800">
+                <h1 className="text-white">We could not find any result. Please search for another movie</h1>
+            </main>
+        </>
+        )
+    }
 
-    console.log(data.results)
-    
 
   return (
     <>
         <Head>
             <title>{userInput} - Search Results</title>
-            <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+            <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
         </Head>
         <main className="flex flex-wrap min-h-[85vh] bg-slate-800">
             {data.results.map((movie:movieCard) => {
